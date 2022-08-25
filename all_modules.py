22ee5f6120d411268857e724b9e5beea2a45a348
MananKateshiya <manankateshiya@gmail.com>
2022-08-25 13:56:57 +0530

    Editable Email Receiver

--- all_modules.py
+++ all_modules.py
@@ -11,19 +11,45 @@ CACHE_PATH = "S:\\Major Project\\Coding\\TTSCache"
 def emailfunc():
 
     #ask for receiver
-    tts2 = gTTS(text='who is the Email receiver?', lang='en')
-    tts2.save(f'{CACHE_PATH}\\e_rec.mp3')
+    def label1():
+        global rec
+        tts2 = gTTS(text='who is the Email receiver?', lang='en')
+        tts2.save(f'{CACHE_PATH}\\e_rec.mp3')
+        mixer.init()
+        mixer.music.load(f'{CACHE_PATH}\\e_rec.mp3')
+        mixer.music.play()
+        ra = sr.Recognizer()
+        with sr.Microphone(device_index=1) as source2:
+            ra.adjust_for_ambient_noise(source2, duration=2)
+            print('Speak Email Receiver ○')
+            a_receiver = ra.listen(source2)
+        rec = (ra.recognize_google(a_receiver))
+        rec = rec.replace('at the rate', '@').replace(' ', '').lower()
+        print(rec)
+    label1()
+            #ask if want to change the receiver
+    ask_r = gTTS(text='Do you want to change this?', lang='en')
+    ask_r.save(f'{CACHE_PATH}\\ask_r.mp3')
     mixer.init()
-    mixer.music.load(f'{CACHE_PATH}\\e_rec.mp3')
+    mixer.music.load(f'{CACHE_PATH}\\ask_r.mp3')
     mixer.music.play()
-    ra = sr.Recognizer()
-    with sr.Microphone(device_index=1) as source2:
-        ra.adjust_for_ambient_noise(source2, duration=2)
-        print('Speak Email Receiver ○')
-        a_receiver = ra.listen(source2)
-    rec = (ra.recognize_google(a_receiver))
-    rec = rec.replace('at the rate', '@').replace(' ', '').lower()
-    print(rec)
+    askR = sr.Recognizer()
+    with sr.Microphone(device_index=1) as sAsk:
+        askR.adjust_for_ambient_noise(sAsk, duration=2)
+        print('Do you want to change this? ○')
+        ans_f = askR.listen(sAsk)
+    ans = (askR.recognize_google(ans_f)).lower()   
+    print(ans) 
+    if ans == 'yes':
+        label1()
+        # ra = sr.Recognizer()
+        # with sr.Microphone(device_index=1) as source2:
+        #     ra.adjust_for_ambient_noise(source2, duration=2)
+        #     print('Speak Email Receiver ○')
+        #     a_receiver = ra.listen(source2)
+        # rec = (ra.recognize_google(a_receiver))
+        # rec = rec.replace('at the rate', '@').replace(' ', '').lower()
+        # print(rec)
 
     #ask subject
     tts = gTTS(text='what is the Email subject?', lang='en')
@@ -34,7 +60,7 @@ def emailfunc():
     rb = sr.Recognizer()
     with sr.Microphone(device_index=1) as source3:
         rb.adjust_for_ambient_noise(source3, duration=2)
-        print('Speak Emali Subject ○')
+        print('Speak Email Subject ○')
         a_subject = rb.listen(source3)
     sub = (rb.recognize_google(a_subject)).lower()
     
@@ -53,7 +79,7 @@ def emailfunc():
     body = (rc.recognize_google(a_receiver))
     
     #main Email logic
-    print(f"From: manan.kateshiya111006@marwadiuniversity.ac.in\nTo: {rec}\nSubject: {sub} \nBody: {body}")
+    print(f"\n\nFrom: manan.kateshiya111006@marwadiuniversity.ac.in\nTo: {rec}\nSubject: {sub} \nBody: {body}")
     semsg(rec, sub, body)
     tts3 = gTTS(text='Your Email has been sent successfully!', lang='en')
     tts3.save(f'{CACHE_PATH}\\e_success.mp3')
@@ -68,3 +94,6 @@ def greet():
     mixer.init()
     mixer.music.load(f'{CACHE_PATH}\\hello.mp3')
     mixer.music.play()
+
+
+    
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
+from gtts import gTTS
+from pygame import mixer
 
-rec = "manankatesiy"
-sub = "the subject"
-body = "this is the body"
-print(f"From: manan.kateshiya111006@marwadiuniversity.ac.in\nTo: {rec}\nSubject: {sub} \nBody: {body}")
+CACHE_PATH = "S:\\Major Project\\Coding\\TTSCache"
+
+ask_r = gTTS(text='Do you want to change this?', lang='en')
+ask_r.save(f'{CACHE_PATH}\\ask_r.mp3')
+mixer.init()
+mixer.music.load(f'{CACHE_PATH}\\ask_r.mp3')
+mixer.music.play()
